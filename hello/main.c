87d3301b92b9045871e2a73c36d97125e14baee3
ddemidov <ddemidov@yandex-team.ru>
2016-05-23 00:04:18 +0300

    first week done

--- hello/main.c
+++ hello/main.c
@@ -1,7 +1,27 @@
-#include "hello.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <dlfcn.h>
+
+void (*hello_message)(const char *);
+
+bool init_library() {
+	void *hdl = dlopen("./libHello.so", RTLD_LAZY);
+	if (NULL == hdl)
+	{
+		return false;
+	}
+	hello_message = (void (*)(const char*))dlsym(hdl, "hello_message");
+	return true;
+}
 
 int main()
 {
-	hello_message("Dima");
+	if (init_library()) {
+		hello_message("Dima");
+	}
+	else {
+		printf("Error!\n");
+	}
 	return 0;
 }
