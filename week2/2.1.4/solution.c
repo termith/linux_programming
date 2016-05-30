5feab0261fa94e945fbf3010f470e232ff4d1cc2
ddemidov <ddemidov@yandex-team.ru>
2016-05-30 20:03:23 +0300

    2.1.4 done

--- week2/2.1.4/solution.c
+++ week2/2.1.4/solution.c
@@ -9,7 +9,7 @@ int option_index;
 int main(int argc, char *argv[])
 {	
 	opterr = 0;
-	const char* short_options = "qiv";
+	const char* short_options = "q:i::v::";
 
 	const struct option long_options[] = {
 		{"query",optional_argument,NULL,'q'},
@@ -18,14 +18,15 @@ int main(int argc, char *argv[])
 		{NULL,0,NULL,0}
 	};
 
-	while ((rez=getopt_long_only(argc,argv,short_options,
+	while ((rez=getopt_long(argc,argv,short_options,
 		long_options,&option_index))!=-1){
 
 		switch(rez){
 			case 'q': {
-				if (optarg == NULL)
+				if (optarg == NULL) {
 					result = "-";
-				break;
+					break;
+				}
 			};
 			case 'i':
 			case 'v':{
