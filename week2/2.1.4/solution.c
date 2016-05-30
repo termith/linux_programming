6dacf76790631c20890c27ddb3580b2ba6767131
ddemidov <ddemidov@yandex-team.ru>
2016-05-30 18:46:17 +0300

    wrong solution for 2.1.4

--- week2/2.1.4/solution.c
+++ week2/2.1.4/solution.c
@@ -1,1 +1,43 @@
 #include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+char* result = "+";
+int rez;
+int option_index;
+
+int main(int argc, char *argv[])
+{	
+	opterr = 0;
+	const char* short_options = "qiv";
+
+	const struct option long_options[] = {
+		{"query",optional_argument,NULL,'q'},
+		{"longinformationrequest",optional_argument,NULL,'i'},
+		{"version",optional_argument,NULL,'v'},
+		{NULL,0,NULL,0}
+	};
+
+	while ((rez=getopt_long_only(argc,argv,short_options,
+		long_options,&option_index))!=-1){
+
+		switch(rez){
+			case 'q': {
+				if (optarg == NULL)
+					result = "-";
+				break;
+			};
+			case 'i':
+			case 'v':{
+				continue;
+			};
+	
+			default: {
+				result = "-";
+				break;
+			};
+		};
+	};
+	printf("%s\n", result);
+	return 0;
+}
